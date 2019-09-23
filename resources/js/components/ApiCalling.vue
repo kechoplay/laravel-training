5502cd76af75323f7b5d1b359b36ddce752c43e3
tungks <tungks@watermelon.vn>
2019-09-23 17:36:35 +0700

    axios

--- resources/js/components/ApiCalling.vue
+++ resources/js/components/ApiCalling.vue
@@ -1,10 +1,42 @@
 <template>
-    
+    <div class="api-calling">
+        <div class="create-form">
+            <div class="product-name-input">
+                <input type="text" v-model="product.name">
+            </div>
+            <div class="product-name-input">
+                <input type="text" v-model.number="product.price">
+            </div>
+            <div class="button-create">
+                <button @click="createProduct">Create</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "ApiCalling"
+        name: "ApiCalling",
+        data() {
+            return {
+                product: {
+                    name: '',
+                    price: 0
+                }
+            }
+        },
+        methods: {
+            createProduct() {
+                axios.post('/products', {
+                    name: this.product.name,
+                    price: this.product.price
+                }).then(response => {
+                    console.log(response.data.result)
+                }).catch(error => {
+                    console.log(error)
+                })
+            }
+        }
     }
 </script>
 
